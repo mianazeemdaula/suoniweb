d109690b45ad97b47949de23b2aca937e4213c6c
mianazeemdaula <mianazeemdaula@gmail.com>
2023-09-03 18:20:18 +0500

    s

--- app/Http/Controllers/Api/PaymentHooksController.php
+++ app/Http/Controllers/Api/PaymentHooksController.php
@@ -6,12 +6,16 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
+// Stripe
+use Stripe\Stripe;
+use Stripe\PaymentIntent;
+
 class PaymentHooksController extends Controller
 {
     function stripePayment(Request $request) {
         if($request->id) {
-            \Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
-            $paymentIntent = \Stripe\PaymentIntent::retrieve(
+            Stripe::setApiKey(env('STRIPE_SECRET'));
+            $paymentIntent = PaymentIntent::retrieve(
                 $request->id
             );
             Log::debug($paymentIntent);
