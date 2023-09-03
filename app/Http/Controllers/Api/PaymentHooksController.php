9422099c18359411041462719526d1e19debca30
mianazeemdaula <mianazeemdaula@gmail.com>
2023-09-03 18:59:49 +0500

    da

--- app/Http/Controllers/Api/PaymentHooksController.php
+++ app/Http/Controllers/Api/PaymentHooksController.php
@@ -20,7 +20,7 @@ class PaymentHooksController extends Controller
             PaymentGatwayLog::create([
                 'gatway_name' => 'stripe',
                 'response' => $event->all(),
-                'data' => $event->data->object->metadata,
+                'data' => $event->data,
                 'status' => $event->type,
             ]);
         }
