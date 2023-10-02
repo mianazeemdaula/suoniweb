3669b3e5dce835d76539d2b98947ca9a13345d42
mianazeemdaula <mianazeemdaula@gmail.com>
2023-10-02 13:07:38 +0500

    s

--- app/Http/Controllers/Api/PaymentHooksController.php
+++ app/Http/Controllers/Api/PaymentHooksController.php
@@ -24,7 +24,7 @@ class PaymentHooksController extends Controller
                 'data' => $event->data['object']['metadata'],
                 'status' => $event->type,
             ]);
-            $log = PaymentGatwayLog::whereJsonContains('response->id', $event->id)
+            $log = PaymentGatwayLog::whereJsonContains('id', $event->id)
             ->where('status', $event->type)
             ->first();
             switch ($event->type) {
@@ -56,7 +56,7 @@ class PaymentHooksController extends Controller
                     $group_lesson->save();
                 }
               }
-              if($metadata['type'] == 'topup' && $log != null) {
+              if($metadata['type'] == 'topup' && $log == null) {
                 $userId = $metadata['user_id'];
                 $amount = $event->data['object']['amount'];
                 User::find($userId)->updateBalance(($amount / 100), $userId, 'Topup');
