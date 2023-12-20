7e13077681cb9deb3dc7f22d8e71630f9de0c6dd
mianazeemdaula <mianazeemdaula@gmail.com>
2023-12-20 13:06:07 +0500

    as

--- resources/views/app/stripe_connect.blade.php
+++ resources/views/app/stripe_connect.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     {{-- center of the screen using tailwind --}}
-    <div class="flex justify-center">
+    <div class="flex justify-center h-screen">
         <div class="w-1/2 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <a href="suoni://app.gleedu.com/bank/{{ $account }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                     Connect with Stripe
