b75b2604a3cd048d88294337a6131f7bb29170ea
mianazeemdaula <mianazeemdaula@gmail.com>
2024-02-01 11:19:20 +0500

    as

--- config/laratex.php
+++ config/laratex.php
@@ -2,10 +2,10 @@
 
 return [
 	// bin path to your pdflatex installation | use 'which pdflatex' on a linux system to find out which is the path to your pdflatex installation
-	'binPath' => 'pdflatex',
+	'binPath' => '/usr/bin/pdflatex',
 
 	// bin path to your bibtex installation | use 'which bibtex' on a linux system to find out which is the path to your bibtex installation
-	'bibTexPath' => 'bibtex',
+	'bibTexPath' => '/usr/bin/bibtex',
 
 	// Folder in your storage folder where you would like to store the temp files created by LaraTeX
 	'tempPath' => 'app/',
