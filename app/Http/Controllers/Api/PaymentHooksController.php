9be7bfd41fc62c9bbb2bbb430cffba4afee5fff0
mianazeemdaula <mianazeemdaula@gmail.com>
2024-03-06 19:38:14 +0500

    break

--- app/Http/Controllers/Api/PaymentHooksController.php
+++ app/Http/Controllers/Api/PaymentHooksController.php
@@ -38,69 +38,77 @@ class PaymentHooksController extends Controller
                 'status' => $event->type,
             ]);
             switch ($event->type) {
-            case 'payment_intent.amount_capturable_updated':
-              $paymentIntent = $event->data['object'];
-            case 'payment_intent.canceled':
-              $paymentIntent = $event->data['object'];
-            case 'payment_intent.created':
-              $paymentIntent = $event->data['object'];
-            case 'payment_intent.partially_funded':
-              $paymentIntent = $event->data['object'];
-            case 'payment_intent.payment_failed':
-              $paymentIntent = $event->data['object'];
-            case 'payment_intent.processing':
-              $paymentIntent = $event->data;
-            case 'payment_intent.requires_action':
-              $paymentIntent = $event->data['object'];
-            case 'payment_intent.succeeded':
-              $metadata = $event->data['object']['metadata'];
-              if($metadata['type'] == 'lessons') {
-                $lessonIds = [];
-                // foreach(json_decode($metadata['lessons']) as $l) {
-                //     $lesson = \App\Models\Lession::find($l);
-                //     $lesson->fee_paid = true;
-                //     $lesson->save();
-                //     $lessonIds[] = $lesson->id;
-                // }
-                // foreach(json_decode($metadata['group_lessons']) as $gl) {
-                //     $group_lesson = \App\Models\GroupUser::find($gl);
-                //     $group_lesson->fee_paid = true;
-                //     $group_lesson->save();
-                //     $lessonIds[] = $gl->lesson->id;
-                // }
+              case 'payment_intent.amount_capturable_updated':
+                $paymentIntent = $event->data['object'];
+                break;
+              case 'payment_intent.canceled':
+                $paymentIntent = $event->data['object'];
+                break;
+              case 'payment_intent.created':
+                $paymentIntent = $event->data['object'];
+                break;
+              case 'payment_intent.partially_funded':
+                $paymentIntent = $event->data['object'];
+                break;
+              case 'payment_intent.payment_failed':
+                $paymentIntent = $event->data['object'];
+                break;
+              case 'payment_intent.processing':
+                $paymentIntent = $event->data;
+                break;
+              case 'payment_intent.requires_action':
+                $paymentIntent = $event->data['object'];
+                break;
+              case 'payment_intent.succeeded':
+                $metadata = $event->data['object']['metadata'];
+                if($metadata['type'] == 'lessons') {
+                  $lessonIds = [];
+                  // foreach(json_decode($metadata['lessons']) as $l) {
+                  //     $lesson = \App\Models\Lession::find($l);
+                  //     $lesson->fee_paid = true;
+                  //     $lesson->save();
+                  //     $lessonIds[] = $lesson->id;
+                  // }
+                  // foreach(json_decode($metadata['group_lessons']) as $gl) {
+                  //     $group_lesson = \App\Models\GroupUser::find($gl);
+                  //     $group_lesson->fee_paid = true;
+                  //     $group_lesson->save();
+                  //     $lessonIds[] = $gl->lesson->id;
+                  // }
 
-                // foreach ($lessonIds as $id) {
-                //   $noti = Notifications::whereJsonContains('data->id', $id)
-                //   ->whereJsonContains('data->type','lession')
-                //   ->where('queued',true)->first();
-                //   if($noti){
-                //     Fcm::sendNotification($noti);
-                //     $noti->queued = false;
-                //     $noti->save();
-                //   }
-                // }
-              }else if($metadata['type'] == 'topup') {
-                $userId = $metadata['user_id'];
-                $amount = $event->data['object']['amount'];
-                $currency = strtoupper($event->data['object']['currency'] ?? 'usd');
-                $amount = ($amount / 100);
-                $rate = Currency::whereName($currency)->first();
-                if($rate){
-                  $amount = $amount / $rate->rate;
+                  // foreach ($lessonIds as $id) {
+                  //   $noti = Notifications::whereJsonContains('data->id', $id)
+                  //   ->whereJsonContains('data->type','lession')
+                  //   ->where('queued',true)->first();
+                  //   if($noti){
+                  //     Fcm::sendNotification($noti);
+                  //     $noti->queued = false;
+                  //     $noti->save();
+                  //   }
+                  // }
+                }else if($metadata['type'] == 'topup') {
+                  $userId = $metadata['user_id'];
+                  $amount = $event->data['object']['amount'];
+                  $currency = strtoupper($event->data['object']['currency'] ?? 'usd');
+                  $amount = ($amount / 100);
+                  $rate = Currency::whereName($currency)->first();
+                  if($rate){
+                    $amount = $amount / $rate->rate;
+                  }
+                  $user = User::find($userId);
+                  $last = $user->transactions()->where('tx_id', $event->data['object']['id'])->first();
+                  if(!$last){
+                    $meta = [
+                      'tx_id' => $event->data['object']['id'],
+                      'tx_amount' => ($event->data['object']['amount'] / 100),
+                      'tx_currency' => $currency,
+                    ];
+                    User::find($userId)->updateBalance($amount, $userId, 'Topup', true, $meta);
+                  }
                 }
-                $user = User::find($userId);
-                $last = $user->transactions()->where('tx_id', $event->data['object']['id'])->first();
-                if(!$last){
-                  $meta = [
-                    'tx_id' => $event->data['object']['id'],
-                    'tx_amount' => ($event->data['object']['amount'] / 100),
-                    'tx_currency' => $currency,
-                  ];
-                  User::find($userId)->updateBalance($amount, $userId, 'Topup', true, $meta);
-                }
-              }
-            default:
-              echo 'Received unknown event type ' . $event->type;
+                break;
+              default:
+                echo 'Received unknown event type ' . $event->type;
           }
         }
         
